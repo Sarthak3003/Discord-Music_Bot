cc62cb4949b74146c4e69fa127eb685bf2735ae3
Sarthak Bhan <sarthakcr007@gmail.com>
2022-01-22 22:26:24 +0530

    Finalised Discord Bot with Basic Functions for Urmi

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+import music
+
+cogs = [music]
+
+client = commands.Bot(command_prefix='horm!', intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+  cogs[i].setup(client)
+
+# token = os.environ.get("TOKEN")
+
+token=""
+with open("token.txt") as file:
+    token=file.read()
+
+client.run(token)
